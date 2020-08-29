7a181ec66b4d3b1b8360d38e9aa01616a9dd7f8b
brgotcher <brgotcher@gmail.com>
2020-08-29 12:40:31 -0500

    change strings2.py

--- strings2.py
+++ strings2.py
@@ -2,11 +2,20 @@
 #         012345678901234
 parrot = "Norwegian Blue"
 
-print(parrot)
+print(parrot[0:6])  # Norweg
+print(parrot[3:5])
+print(parrot[0:9])
+print(parrot[:9])
 
-print(parrot[3])
-print(parrot[4])
-print(parrot[9])
-print(parrot[3])
-print(parrot[6])
-print(parrot[8])
+print(parrot[10:])
+
+print(parrot[:6])
+print(parrot[6:])
+
+print(parrot[:6] + parrot[6:])
+
+print(parrot[:])
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+print(letters[-1:-27:-1])
