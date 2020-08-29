4b5e56b3e644a911191783149027eea9249fc515
brgotcher <brgotcher@gmail.com>
2020-08-29 12:43:17 -0500

    change strings2.py

--- strings2.py
+++ strings2.py
@@ -3,19 +3,7 @@
 parrot = "Norwegian Blue"
 
 print(parrot[0:6])  # Norweg
-print(parrot[3:5])
-print(parrot[0:9])
-print(parrot[:9])
+print(parrot[-14:-8])
 
-print(parrot[10:])
-
-print(parrot[:6])
-print(parrot[6:])
-
-print(parrot[:6] + parrot[6:])
-
-print(parrot[:])
-
-letters = "abcdefghijklmnopqrstuvwxyz"
-
-print(letters[-1:-27:-1])
+print(parrot[-4:-2])
+print(parrot[-4:12])
