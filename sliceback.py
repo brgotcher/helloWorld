ca99762447eada84c605127621b7b6a1b3f523cc
brgotcher <brgotcher@gmail.com>
2020-09-01 16:07:20 -0500

    add sliceback.py

---
+++ sliceback.py
@@ -0,0 +1,18 @@
+letters = ""
+
+backwards = letters[::-1]
+print(backwards)
+
+qpo = letters[16:13:-1]
+edcba = letters[4::-1]
+last8 = letters[:-9:-1]
+
+print(qpo)
+print(edcba)
+print(last8)
+
+
+print(letters[-4:])
+print(letters[-1:])
+print(letters[:1])
+print(letters[0])
